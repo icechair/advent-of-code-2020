7ddaef5b0c05a0481515e5f8abcb605ceac064c7
Martin Gabel <martin.gabel@icechair.de>
2020-12-02 13:07:54 +0100

    gitignore ...

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/home/freek/Projects/advent-of-code-2020/day2/target/debug/day2: /home/freek/Projects/advent-of-code-2020/day2/src/main.rs
--- day2/target/rls/debug/deps/day2-ea0da56bdee3d4d6.d
+++
@@ -1,5 +0,0 @@
-/home/freek/Projects/advent-of-code-2020/day2/target/rls/debug/deps/day2-ea0da56bdee3d4d6.rmeta: src/main.rs
-
-/home/freek/Projects/advent-of-code-2020/day2/target/rls/debug/deps/day2-ea0da56bdee3d4d6.d: src/main.rs
-
-src/main.rs:
