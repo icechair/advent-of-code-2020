dd24656de87acfa5cc3db8bef1ae4566e187ba21
Martin Gabel <martin.gabel@icechair.de>
2020-12-03 11:26:15 +0100

    progress on day3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -11,10 +11,45 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
+fn check_slope(left: usize, down: usize, input: &String) -> usize {
+    let mut n_trees = 0;
+    let mut width = 0;
+    let mut x = left;
+    let mut y = 0;
+
+    if let Ok(lines) = read_lines(&input) {
+        for line in lines {
+            if let Ok(row) = line {
+                if width == 0 {
+                    width = row.len()
+                }
+                if y < down {
+                    y += 1;
+                    continue;
+                }
+                y = 0;
+                let chars = row.chars().collect::<Vec<char>>();
+                if chars[x] == '#' {
+                    n_trees += 1;
+                }
+                x += left;
+                if x >= width {
+                    x -= width;
+                }
+            }
+        }
+    }
+    n_trees
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let mut n_trees = 0;
+
+    n_trees = check_slope(3, 1, &args[1]);
+
+    /*
     let mut width = 0;
     let mut x = 0;
     if let Ok(lines) = read_lines(&args[1]) {
@@ -36,5 +71,6 @@ fn main() {
             }
         }
     }
+    */
     println!("{}", n_trees)
 }
