f74f260d2676125d346f05171a1ea7feac16679f
Martin Gabel <martin.gabel@icechair.de>
2020-12-07 14:39:33 +0100

    day7 part2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,7 +2,10 @@ use petgraph::algo;
 use petgraph::dot::Dot;
 use petgraph::graph::Graph;
 use petgraph::graph::NodeIndex;
+use petgraph::visit::Dfs;
 use petgraph::visit::IntoNodeReferences;
+use petgraph::Outgoing;
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
@@ -21,7 +24,7 @@ fn find_node_index(graph: &Graph<String, usize>, item: &str) -> Option<NodeIndex
     return None;
 }
 
-fn part1(file: &str) -> usize {
+fn build_graph(file: &str) -> Graph<String, usize> {
     let mut graph = Graph::<String, usize>::new();
     let reader = BufReader::new(File::open(file).expect("File::open failed"));
     for line in reader.lines() {
@@ -68,30 +71,44 @@ fn part1(file: &str) -> usize {
             }
         }
     }
-    let start = find_node_index(&graph, "shiny gold");
-    let mut n_bags = 0;
-    if let Some(start) = start {
-        for (id, n) in graph.node_references() {
-            if id != start {
-                if algo::has_path_connecting(&graph, id, start, None) {
-                    n_bags += 1;
-                }
-            }
-        }
-    }
-    //    println!("{}", Dot::new(&graph));
-    println!("{}", n_bags);
-    return 0;
+
+    return graph;
 }
 
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
+    let graph = build_graph(&args[1]);
+    let start = find_node_index(&graph, "shiny gold");
 
-    if &args[2] == "1" {
-        println!("{}", part1(&args[1]));
+    if let Some(start) = start {
+        if &args[2] == "1" {
+            let mut n_bags = 0;
+            for (id, n) in graph.node_references() {
+                if id != start {
+                    if algo::has_path_connecting(&graph, id, start, None) {
+                        n_bags += 1;
+                    }
+                }
+            }
+            println!("{}", n_bags);
+        } else if &args[2] == "2" {
+            println!("{}", walk_graph(&graph, start) - 1);
+        }
     }
+
     Ok(())
 }
+
+fn walk_graph(graph: &Graph<String, usize>, node: NodeIndex) -> usize {
+    let mut neighbors = graph.neighbors_directed(node, Outgoing).detach();
+    let mut sum = 1;
+    while let Some((edge, next)) = neighbors.next(&graph) {
+        let product = graph[edge] * walk_graph(&graph, next);
+        sum += product;
+    }
+    return sum;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
